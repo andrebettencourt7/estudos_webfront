e5dda72e0ca832a88817757df75e9d3bf8ae0a1e
andrebettencourt7 <andreluispb10@gmail.com>
2023-07-26 21:48:23 -0300

    teste de dados

---
+++ script.js
@@ -0,0 +1,14 @@
+//tipos de dados
+var nome = "André";
+let idade = 23;
+const anoNasc = 2000;
+let idadeNamorada = "23";
+
+/*
+= atribuição 
+== comparar se é igual
+=== comparar se é exatamente igual
+*/
+
+console.log(idade === idadeNamorada);
+
