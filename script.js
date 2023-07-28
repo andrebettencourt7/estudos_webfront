eb94003d66675015680f58546a5d7f669ad174f1
andrebettencourt7 <andreluispb10@gmail.com>
2023-07-27 21:48:25 -0300

    estudando funções

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
-//tipos de dados
-var nome = "André";
-let idade = 23;
-const anoNasc = 2000;
-let idadeNamorada = "23";
-
-/*
-= atribuição 
-== comparar se é igual
-=== comparar se é exatamente igual
-*/
-
-//console.log(idade === idadeNamorada);
-
-/*
-let listaCompras = [];
-
-listaCompras.push("Maça", "Pêra");
 
 
-console.log(listaCompras.length);
-console.log(listaCompras);
-*/
+const titulo = document.getElementById("titulo");
+const nomeInput = document.getElementById("nome");
+const emailInput = document.getElementById("email");
+const telefoneInput = document.getElementById("telefone");
+const result_nome = document.getElementById("result_nome");
+const result_email = document.getElementById("result_email");
+const result_telefone = document.getElementById("result_telefone");
 
-//Array 
+titulo.textContent = "Senai Cotia 2023"
 
-/* Acessamos o array fazendo ref ao index
-*/
-
-let lista = [1, 2, 3, 4, 5, 6]
-
-for (let index = 0; index < lista.length; index++) {
-    const element = lista[index];
-
-    console.log(element);
-};
-
-
+function salvar() {
+    result_nome.textContent = nomeInput.value
+    result_email.textContent = emailInput.value
+    result_telefone.textContent = telefoneInput.value
+}
