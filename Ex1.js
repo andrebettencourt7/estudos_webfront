2c1966c76541dd92f339c934943753a31bb5eac8
andrebettencourt7 <andreluispb10@gmail.com>
2023-08-01 19:36:53 -0300

    Exercicio 1: sistema de compra de mercado

---
+++ Ex1.js
@@ -0,0 +1,40 @@
+const pergunta = document.getElementById("pergunta");
+const qtdMorango = document.getElementById("qtdMorango");
+const qtdMaca = document.getElementById("qtdMaca");
+const valorFinal = document.getElementById("valor");
+var valorMaca
+var valorMorango
+var valorDesc
+var kiloTotal
+
+
+pergunta.textContent = "Quantos kilos ira comprar?"
+valorFinal.textContent = "Valor total"
+
+function comprar() {
+
+    if (qtdMorango.value <= 5) {
+        valorMorango = qtdMorango.value * 2.50;
+
+    } else {
+        valorMorango = qtdMorango.value * 2.20;
+    }
+
+    if (qtdMaca.value <= 5) {
+        valorMaca = qtdMaca.value * 1.80;
+
+    } else {
+        valorMaca = qtdMaca.value * 1.50;
+    }
+
+    valorFinal.textContent = valorMaca + valorMorango;
+
+    valorDesc = valorFinal.textContent*0.1;
+
+    kiloTotal = qtdMaca + qtdMorango;
+
+    if(valorFinal.textContent >= 25 || kiloTotal > 8){
+        valorFinal.textContent = valorFinal.textContent - valorDesc;
+    }
+
+}
--- calc.js
+++ calc.js
@@ -1,7 +1,7 @@
 const numero1 = document.getElementById("number1");
 const numero2 = document.getElementById("number2");
 const resultado = document.getElementById("resultado");
-const porcent = document.getElementById("porcent");
+
 
 resultado.textContent = "Resultado";
 porcent.textContent = "*No caso de porcentagem, colocar o valor q deseja saber no primeiro campo e quanto e o porcento no segundo"
