a4a0c3f9597ad6373af97ed761a85ba977754578
andrebettencourt7 <andreluispb10@gmail.com>
2023-08-02 21:57:45 -0300

    Fazendo um cadastro simples

--- listener.js
+++ listener.js
@@ -7,17 +7,54 @@ const titulo = document.getElementById("titulo");
 const minhaDiv = document.getElementById("minha-div");
 const buttonAdd = document.getElementById("button");
 const userName = document.getElementById("nome");
+const userEmail = document.getElementById("email");
+const userProfissao = document.getElementById("profissao");
+const feminino = document.getElementById("fam");
+const masculino = document.getElementById("masc");
 
-let idade = 26
 
 buttonAdd.addEventListener('click', () => {
-    minhaDiv.innerHTML = `
+   
 
-        <h2>${idade}</h2>
-        <button>Pode clicar...</button>
+    const novaTag = document.createElement("div");
+    novaTag.classList.add("flip-card")
 
+ 
+    if(masculino.checked === true){
+        novaTag.innerHTML = `
+        <div class="flip-card-inner">
+            <div class="flip-card-front">
+                <img src="./avatar/avatar2.jpg" alt="Avatar" style="width:300px;height:300px;">
+            </div>
+            <div class="flip-card-back">
+                <h1>${userName.value}</h1>
+                <p>${userEmail.value}</p>
+                <p>${userProfissao.value}</p>
+            </div>
+        </div>
+        
     `
-    
+    }else if(masculino.checked === false){
+        novaTag.innerHTML = `
+        <div class="flip-card-inner">
+            <div class="flip-card-front">
+                <img src="./avatar/avatar3.jpg" alt="Avatar" style="width:300px;height:300px;">
+            </div>
+            <div class="flip-card-back">
+                <h1>${userName.value}</h1>
+                <p>${userEmail.value}</p>
+                <p>${userProfissao.value}</p>
+            </div>
+        </div>
+        
+    `
+    }else{
+        alert("Precisa escolher algum gênero");
+    }
+
+
+    minhaDiv.appendChild(novaTag);
+
 })
 
 
